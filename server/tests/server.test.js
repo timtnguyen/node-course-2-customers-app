bc7e036d11877e26a85c010c5877ebad0340abce
timtnguyen <nguyen710150@yahoo.com>
2018-07-24 13:50:41 -0700

    Add to GET /customers

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,12 +4,20 @@ const request = require('supertest');
 const {app} = require('./../server'); 
 const {Customer} = require('./../models/customer'); 
 
+const customers = [{
+    firstName: 'Tim'
+}, {
+    firstName: 'Kha'
+}];
+
 beforeEach((done) => {
-    Customer.remove({}).then(() => done());
+    Customer.remove({}).then(() => {
+        return Customer.insertMany(customers); 
+    }).then(() => done());
 }); 
 
 describe('POST /customers', () => {
-    it('should create a new todo', (done) => {
+    it('should create a new customer', (done) => {
         let firstName = 'Huong'; 
 
         request(app)
@@ -24,7 +32,7 @@ describe('POST /customers', () => {
                     return done(err); 
                 }
 
-                Customer.find().then((customers) => {
+                Customer.find({firstName}).then((customers) => {
                     expect(customers.length).toBe(1); 
                     expect(customers[0].firstName).toBe(firstName); 
                     done(); 
@@ -43,10 +51,22 @@ describe('POST /customers', () => {
                 }
 
                 Customer.find().then((customers) => {
-                    expect(customers.length).toBe(0); 
+                    expect(customers.length).toBe(2); 
                     done(); 
                 }).catch((err) => done(err)); 
             });
 
+    });
+});
+
+describe('GET /customers', () => {
+    it('should get all customers', (done) => {
+        request(app)
+            .get('/customers')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.customers.length).toBe(2); 
+            })
+            .end(done); 
     });
 });
