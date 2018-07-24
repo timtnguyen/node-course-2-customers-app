047f5fd6d4b7527d0f3959b3c3646db4a35ae3ec
timtnguyen <nguyen710150@yahoo.com>
2018-07-23 23:21:49 -0700

    Created new file update

---
+++ playground/mongodb-update.js
@@ -0,0 +1,37 @@
+const { MongoClient, ObjectID } = require('mongodb'); 
+
+MongoClient.connect('mongodb://localhost/CustomerApp', (err, client) => {
+    if (err) {
+        return console.log('Error', err); 
+    }
+
+    console.log('Connected to MongoDB server'); 
+    const db = client.db('CustomerApp'); 
+
+    // db.collection('Customers').findOneAndUpdate({
+    //     _id: new ObjectID('5b56b747a8dd9d5758053804')
+    // }, {
+    //     $set: {
+    //         firstName: 'Tim'
+    //     }
+    // }, {
+    //     returnOriginal: 'Tim'
+    // }).then((result) => {
+    //     console.log(result);
+    // }); 
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5b5680d48afab4028fa789d1')
+    }, {
+        $set: {
+            name: 'Huong Khuu'
+        }, 
+        $inc: {
+            age: 5 
+        }
+    }, {
+        returnOriginal: false 
+    }).then((result) => {
+        console.log(result); 
+    });
+});
